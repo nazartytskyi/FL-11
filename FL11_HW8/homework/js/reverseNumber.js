7e0d2e46767b6e47e314dfb3ed2ab70770bca3ae
Nazar Tytskyi <nazartitskiy@gmail.com>
2019-07-14 20:17:32 +0300

    Update reverseNumber.js

--- FL11_HW8/homework/js/reverseNumber.js
+++ FL11_HW8/homework/js/reverseNumber.js
@@ -4,7 +4,7 @@ function reverseNumber(number) {
 	while (number / 10 % 10 !== 0) {
 		reverse *= 10;
 		reverse += number % 10;
-		number = Math.sign(number) * Math.floor( Math.abs(number / 10) ); //- number / 10 % 1;
+		number = Math.sign(number) * Math.floor( Math.abs(number / 10) );
 	}
 	return reverse;
 }
